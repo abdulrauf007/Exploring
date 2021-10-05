3a27e633557febb2758946a373b032130add9386
HP <abdulraufsiddiqui007@gmail.com>
2021-10-05 18:37:41 +0500

    Initial

---
+++ MaxicoNarcos/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace MaxicoNarcos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example-1: Write a C# Sharp program to print Hello and your name in a separate line.
+
+            //Console.WriteLine("Hell");
+            //Console.WriteLine("Abdul Rauf Siddiqui");
+
+
+            //Example -2: Write a C# Sharp program to print the sum of two numbers.
+
+            //Console.WriteLine("Enter the Value 1");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the Value 2");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //var result = a + b;
+            //Console.WriteLine(result);
+
+
+            //Example -3: Write a C# Sharp program to print the result of dividing two numbers.
+
+            //Console.WriteLine("Enter the Value of X");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the Value of Y");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //var result = x / y;
+            //Console.WriteLine(result);
+
+            //Example -4: Write a C# Sharp program to print the result of the specified operations.
+
+            //Console.WriteLine(-1+4*3);
+            //Console.WriteLine((3+4)*10);
+            //Console.WriteLine((3-2)*(34+2));
+            //Console.WriteLine(14+-3*5+2);
+            //Console.WriteLine(33-3/332);
+
+            //Example -5:Write a C# Sharp program to swap two numbers. 
+
+            //Console.WriteLine("Enter The Value 1");
+            //var Value1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value 2");
+            //var Value2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("First Value"+ Value2);
+            //Console.WriteLine("Second Value"+ Value1);
+
+            //Example -6: Write a C# Sharp program to print the output of multiplication of three numbers
+            //which will be entered by the user
+
+            //Console.WriteLine("Enter The Value Of A");
+            //int A = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value of B");
+            //int B = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value of C");
+            //int C = Convert.ToInt32(Console.ReadLine());
+
+            //var Output = A * B * C;
+            //Console.WriteLine(Output);
+
+            //Example -7: Write a C# Sharp program to print on screen the output of
+            //adding, subtracting, multiplying and dividing of two numbers which will be entered by the user
+
+            //Console.WriteLine("Enter The First Value ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Second Value");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Add: {0}+{1}={2}", a, b, a + b);
+            //Console.WriteLine("Sub: {0}-{1}={2}", a, b, a - b);
+            //Console.WriteLine("Mul: {0}*{1}={2}", a, b, a * b);
+            //Console.WriteLine("Div: {0}/{1}={2}", a, b, a / b);
+
+            //Example -8 Write a C# Sharp program that takes a number as input and print its multiplication table.
+
+            //Console.WriteLine("Enter Table");
+            //int TableValue = Convert.ToInt32(Console.ReadLine());
+            //int result;
+
+            //result = 1 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}",TableValue,1,result);
+
+            //result = 2 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 2, result);
+
+            //result = 3 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 3, result);
+
+            //result = 4 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 4, result);
+
+            //result = 5 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 5, result);
+
+            //result = 6 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 6, result);
+
+            //result = 7 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 7, result);
+
+            //result = 8 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 8, result);
+
+            //result = 9 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 9, result);
+
+            //result = 10 * TableValue;
+            //Console.WriteLine("{0} x {1} = {2}", TableValue, 10, result);
+
+            //Example -9: Write a C# Sharp program that takes four numbers as input to calculate and print the average
+
+            //Console.WriteLine( "Enter The Value");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value");
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value");
+            //int d = Convert.ToInt32(Console.ReadLine());
+
+            //var result = (a + b + c + d)/4;
+            //Console.WriteLine(result);
+
+            //Example -10 Write a C# Sharp program to that takes three numbers(x,y,z)
+            //as input and print the output of (x+y).z and x.y + y.z.
+
+            //Console.WriteLine("Enter The Value Of X");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value Of Y");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter The Value Of Z");
+            //int z = Convert.ToInt32(Console.ReadLine());
+
+            //var Result1 = (x + y) * z;
+            //var Result2 = (x * y) + (y * z);
+
+            //Console.WriteLine("Resul 01: ({0}+{1})* {2}: {3}", x,y,z,Result1);
+            //Console.WriteLine("Resul 02: ({0}*{1}) + ({1}*{2}): {3}", x, y, z, Result2);
+
+            //Example -11: 
+
+
+
+
+
+        }
+    }
+}
